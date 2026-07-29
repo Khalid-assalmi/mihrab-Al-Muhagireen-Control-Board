f7ed521c4ff8abb85b2a8ba4533b73c018656d1b
Mont | مونت <assalmi85wrk@gmail.com>
2026-07-29 22:33:36 +0300

    Add files via upload

---
+++ manage.js
@@ -0,0 +1,128 @@
+const Base_url = "https://mihrab-al-muhajireen-default-rtdb.europe-west1.firebasedatabase.app/";
+const dataContainer = document.querySelector(".container");
+
+const urls = {
+    lessons: `${Base_url}lessons.json`,
+    lectures: `${Base_url}lectures.json`,
+    sermons: `${Base_url}sermons.json`
+}
+
+// function to send data to the server.
+async function sendDataToServer(url, data) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        alert("Data sent successfully:");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// Get the Date of today.
+function getDateOfToday() {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = String((now.getMonth() + 1)).padStart(2, "0");
+    let today = String(now.getDate()).padStart(2, "0");
+
+    return today + "/" + month + "/" + year;
+}
+
+// preparation for sending data to the server
+function saveData(dataType, name, status, description) {
+    if (!name || !description) {
+        alert("Please fill in all required fields.");
+        return;
+    } else {
+        const data = {
+            name: name,
+            status: status,
+            description: description,
+            date: getDateOfToday()
+        };
+        // send data to the server.
+        sendDataToServer(urls[dataType], data);
+    }
+}
+
+// function to get data from the server.
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const result = await response.json();
+        if (result && typeof result == 'object' && !Array.isArray(result)) {
+            return result;
+        }
+        return result || [];
+    } catch {
+        console.error(error.message); // print the error massege in console if server don't response or any error else.
+        return null; // return null for print internet disconnected massage in displayData function.
+    }
+}
+
+let arrType = JSON.parse(localStorage.getItem("arr")) || [];
+
+async function displayData(dataType) {
+    var arr = await getData(urls[dataType]);
+
+    localStorage.setItem("arr", JSON.stringify(Object.values(arr)));
+
+    arr = Object.values(arr);
+
+    var id = Object.keys(arr);
+    var randomFirst = Math.floor(Math.random() * 999999);
+    var randomSecond = Math.floor(Math.random() * 999999);
+
+    if (arr === null) { // if (arr) value eqaul null print internet disconnected massage.
+        dataContainer.innerHTML = `
+        <div class="error">
+            <span>
+                <i class="fa-solid fa-wifi"></i>
+                <i class="slash"></i>
+            </span>
+            <h3>أنت غير متصل بالانترنت</h3>
+        </div>
+        `;
+        return; // for stop function.
+    }
+
+    dataContainer.innerHTML = "";
+    if (arr && arr.length > 0) {
+        arr.toReversed().forEach((data, index) => {
+            dataContainer.innerHTML += `
+            <div class="dataCard" onclick="location.href = 'path.html?index=${randomFirst + id[index] + randomSecond}'">
+                <div>
+                    <h4>${index + 1}. ${data.name}</h4>
+                    <small>${data.description}</small>
+                </div>
+                <i class="fas fa-chevron-left"></i>
+            </div>
+        `   ;
+        });
+    } else {
+        dataContainer.innerHTML = `
+            <div class="error">
+                <i class="fa-solid fa-box-open"></i>
+                <h3 class=>لا توجد بيانات مخزنة هنا</h3>
+            </div>
+        `;
+    }
+}
+
+function display() {
+    let queryString = window.location.search;
+
+    let urlParams = new URLSearchParams(queryString);
+    let index = urlParams.get("index");
+
+    document.querySelector("#h3").textContent = arrType[index[6]].name;
+    document.querySelector("#p").textContent = arrType[index[6]].description;
+
+}
---
+++ script.js
@@ -0,0 +1,102 @@
+(function() {
+    const menuCheckBox = document.getElementById("menu");
+
+    let serverPassword = ""; // Variable to store the password from the server.
+    let password = ""; // Keep password varible empty to get password from server.
+    let attempts = 6;
+
+    let passwordInput = document.getElementById("passwordInput");
+    let submitPassword = document.getElementById("submitPassword");
+    let backdrop = document.querySelector(".backdrop");
+    let passwordBox = document.querySelector(".passwordBox");
+    let errBox = document.querySelector(".errorsBox");
+    let correctBox = document.querySelector(".correctBox");
+
+    let passwordBox_isOpen = sessionStorage.getItem("passwordBoxOpen") || "";
+
+    if (!passwordBox_isOpen) {
+        backdrop.style.display = "";
+        passwordBox.style.display = "";
+    } else {
+        backdrop.style.display = "none";
+        passwordBox.style.display = "none";
+    }
+
+    // get password from the server.
+    async function getPassword() {
+        try {
+            const response = await fetch("https://mihrab-al-muhajireen-default-rtdb.europe-west1.firebasedatabase.app/password.json");
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+            const result = await response.json();
+
+            // Assuming the password is the first value in the returned object
+            serverPassword = Object.values(result)[0];
+            password = serverPassword; // Update the password variable with the fetched password.
+            console.log(password)
+        } catch (error) {
+            console.error(error.message);
+            password = null;
+            return;
+        }
+    }
+
+    let div = document.createElement("div");
+    div.classList.add("menu");
+    div.innerHTML = `
+        <a>عدد المستخدمين الحاليين 0</a>
+    `;
+
+    menuCheckBox.addEventListener("change", () => {
+        if (menuCheckBox.checked) {
+            document.body.appendChild(div);
+        } else if(!menuCheckBox.checked) {
+            div.remove();
+        }
+    });
+
+    getPassword(); // Call function for get password.
+
+    window.ononline = () => {
+        getPassword();
+    }
+
+    function checkPassword() {
+        if (!password) return; // Don't check of password if is null.
+        if (attempts > 1) {
+            if (passwordInput.value.trim()) {
+                if (passwordInput.value.trim() === password) {
+                    passwordInput.style.borderColor = "green";
+                    errBox.style.display = "";
+                    correctBox.style.display = "block";
+                    correctBox.textContent = "كلمة المرور صحيحة!";
+                    setTimeout(() => {
+                        backdrop.style.display = "none";
+                        passwordBox.style.display = "none";
+                        sessionStorage.setItem("passwordBoxOpen", "true");
+                    }, 500);
+                } else {
+                    attempts--;
+                    passwordInput.style.borderColor = "red";
+                    passwordInput.value = "";
+                    errBox.style.display = "block";
+                    errBox.textContent = `كلمة المرور غير صحيحة، لديك ${attempts} محاولات متبقية.`;
+                }
+            }
+        } else {
+            passwordInput.disabled = true;
+            passwordInput.value = "";
+            errBox.textContent = "تم حظرك! يرجى المحاولة لاحقاً.";
+        }
+    }
+
+    setInterval(() => {if (passwordBox.style.display == "none" && !passwordBox_isOpen || backdrop.style.display == "none" && !passwordBox_isOpen) location.reload(true);}, 300);
+
+    submitPassword.addEventListener("click", () => {
+        checkPassword();
+    });
+    passwordInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {// Check if the pressed key is "Enter" not any other key.
+            checkPassword();
+        }
+    });
+})();
